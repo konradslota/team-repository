f4af65453b048f618be93430af86816756ce11e4
Hukiz <36443072+Hukiz@users.noreply.github.com>
2018-02-13 12:04:54 +0000

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -4,3 +4,11 @@ public class Welcome{
  System.out.println("Welcome to Github lab 2!");
  }
  }
+
+// for github lab 2
+public class Welcome{
+ public static void main(String[] args){
+ System.out.println("Welcome to Github lab 2!");
+ System.out.println("lbpB now collaborating");
+ }
+}
